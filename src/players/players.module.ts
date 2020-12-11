01ec1cb5ea304f89da169d54cbf600355940faee
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-12-10 19:49:16 -0800

    Formatted project with prettier

--- src/players/players.module.ts
+++ src/players/players.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
-import { PlayersService } from './players.service';
-import { PlayersController } from './players.controller';
+import { Module } from "@nestjs/common";
+import { PlayersService } from "./players.service";
+import { PlayersController } from "./players.controller";
 
 @Module({
-  controllers: [PlayersController],
-  providers: [PlayersService]
+    controllers: [PlayersController],
+    providers: [PlayersService],
 })
 export class PlayersModule {}
