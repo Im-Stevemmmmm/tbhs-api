03c216df5f693c9f463810c716d05fa0a91104c2
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-12-11 00:25:17 -0800

    Added getPitDataFromPlayer

--- src/pit-data/pit-data.service.ts
+++ src/pit-data/pit-data.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from "@nestjs/common";
+import { PitData, Prisma } from "@prisma/client";
 import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
 export class PitDataService {
     constructor(private readonly prisma: PrismaService) {}
+
+    async getPitDataFromPlayer(
+        where: Prisma.PitDataWhereUniqueInput,
+    ): Promise<PitData> {
+        return await this.prisma.pitData.findUnique({ where });
+    }
 }
