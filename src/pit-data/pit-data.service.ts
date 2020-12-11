61058fc8eb164f2415bbf151ee8c78ff18c92930
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-12-11 08:51:05 -0800

    Added create pit data

--- src/pit-data/pit-data.service.ts
+++ src/pit-data/pit-data.service.ts
@@ -7,12 +7,16 @@ export class PitDataService {
     constructor(private readonly prisma: PrismaService) {}
 
     async getAllPitData(): Promise<PitData[]> {
-        return this.prisma.pitData.findMany();
+        return await this.prisma.pitData.findMany();
     }
 
     async getPitDataFromPlayer(
         where: Prisma.PitDataWhereUniqueInput,
     ): Promise<PitData> {
         return await this.prisma.pitData.findUnique({ where });
+    }
+
+    async createPitData(data: Prisma.PitDataCreateInput): Promise<PitData> {
+        return await this.prisma.pitData.create({ data });
     }
 }
