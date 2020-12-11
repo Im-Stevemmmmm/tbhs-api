3018c7a5d92940808fee9bfe894026226ebf7aeb
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-12-10 20:57:31 -0800

    Removed PrismaService from providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from "@nestjs/common";
 import { PlayersModule } from "./players/players.module";
-import { PrismaService } from "./prisma/prisma.service";
 
 @Module({
     imports: [PlayersModule],
-    providers: [PrismaService],
 })
 export class AppModule {}
