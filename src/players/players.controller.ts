e3c625e65e2d0961516f315587b33ae2f0bec731
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-12-10 22:09:40 -0800

    Added register player post route

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 import { Player as PlayerModel } from "@prisma/client";
+import { RegisterPlayerDto } from "./dtos/register-player";
 import { PlayersService } from "./players.service";
 
 @Controller("players")
@@ -14,5 +15,17 @@ export class PlayersController {
     @Get("/:uuid")
     async getPlayerByUuid(@Param("uuid") uuid: string): Promise<PlayerModel> {
         return this.playersService.player({ uuid });
+    }
+
+    @Post()
+    async registerPlayer(
+        @Body() data: RegisterPlayerDto,
+    ): Promise<PlayerModel> {
+        const { uuid } = data;
+
+        return this.playersService.createPlayer({
+            uuid,
+            rank: "noob",
+        });
     }
 }
