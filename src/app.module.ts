852b5944b9de731a162e3570ace16e54022d6283
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-12-10 20:55:36 -0800

    Removed uneeded imports, controllers, and providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { PlayersModule } from "./players/players.module";
-import { PrismaService } from './prisma/prisma.service';
+import { PrismaService } from "./prisma/prisma.service";
 
 @Module({
     imports: [PlayersModule],
-    controllers: [AppController],
-    providers: [AppService, PrismaService],
+    providers: [PrismaService],
 })
 export class AppModule {}
