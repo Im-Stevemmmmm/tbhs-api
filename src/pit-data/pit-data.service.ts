9ccd44c40719a28a8f32ea333caa5c6bced0995a
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-12-11 00:13:56 -0800

    Injected prisma

--- src/pit-data/pit-data.service.ts
+++ src/pit-data/pit-data.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
-export class PitDataService {}
+export class PitDataService {
+    constructor(private readonly prisma: PrismaService) {}
+}
