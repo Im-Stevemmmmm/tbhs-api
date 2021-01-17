839ad1a9f0031137c92a4c6e7b0c1e494ab53f18
Esteban Garcia <estebangarcia2121@gmail.com>
2021-01-16 21:23:20 -0800

    Set redis db to 0

--- src/context.ts
+++ src/context.ts
@@ -2,4 +2,6 @@ import { PrismaClient } from "@prisma/client";
 import Redis from "ioredis";
 
 export const prisma = new PrismaClient();
-export const redis = new Redis();
+export const redis = new Redis({
+    db: 0,
+});
