4f7cb1b779443dd367871f959f4a49d4ca3db3bc
Esteban Garcia <estebangarcia2121@gmail.com>
2021-01-16 20:29:13 -0800

    Added pit stats query params

--- src/routes/game-data/the-pit/index.ts
+++ src/routes/game-data/the-pit/index.ts
@@ -1,31 +1,66 @@
-import { Prisma } from "@prisma/client";
+import { isUUID } from "class-validator";
 import { Router } from "express";
 import { prisma } from "../../../prisma";
-import { capitilize } from "../../../utils/capitilize";
-import { pitStatsRoutes } from "./stats";
+import { adminApiKeyAuth, apiKeyAuth } from "../../../utils/auth-middleware";
+import { createError } from "../../../utils/error";
 
 const router = Router();
 
-router.get("/:uuid", async ({ query: { stats }, params: { uuid } }, res) => {
-    if (stats) {
-        const select: Prisma.PlayerSelect = {};
-        const fieldArray = [].concat(stats as string[]);
+const types = {
+    offensive: prisma.pitOffensiveStats,
+    defensive: prisma.pitDefensiveStats,
+    farming: prisma.pitFarmingStats,
+    miscellaneous: prisma.pitMiscellaneousStats,
+    performance: prisma.pitPerformanceStats,
+    prestige: prisma.pitPrestigeStats,
+    "perks-and-mystics": prisma.pitPerksAndMysticStats,
+};
 
-        fieldArray.forEach(s => (select[`Pit${capitilize(s)}Stats`] = true));
+const getKeys = () => {
+    const keys = Object.keys(types);
+    const finalKey = keys.pop();
 
-        const result = await prisma.player.findFirst({ select });
+    return `${keys.join(", ")} or ${finalKey}`;
+};
 
-        res.send(result);
-    } else {
-        const {
-            uuid: queriedUuid,
-            rank,
-            ...rest
-        } = await prisma.player.findUnique({
+router.get(
+    "/:uuid",
+    apiKeyAuth,
+    async ({ params: { uuid }, query: { type } }, res) => {
+        if (!uuid) {
+            return createError(res, 404, "No UUID provided.");
+        }
+
+        if (!isUUID(uuid)) {
+            return createError(res, 409, "Parameter uuid is not a UUIDv4.");
+        }
+
+        if (type) {
+            const typeArr: string[] = [].concat(type);
+
+            if (typeArr.some(t => !types[t])) {
+                return createError(res, 409, `Type must be ${getKeys()}.`);
+            }
+
+            const data = await Promise.all(
+                typeArr.map(
+                    async t =>
+                        await types[t.toLowerCase()].findUnique({
+                            where: {
+                                playerUuid: uuid,
+                            },
+                        })
+                )
+            );
+
+            return res.send(data);
+        }
+
+        const result = await prisma.player.findUnique({
             where: {
                 uuid,
             },
-            include: {
+            select: {
                 PitDefensiveStats: true,
                 PitFarmingStats: true,
                 PitMiscellaneousStats: true,
@@ -36,10 +71,42 @@ router.get("/:uuid", async ({ query: { stats }, params: { uuid } }, res) => {
             },
         });
 
-        res.send(rest);
-    }
-});
+        if (!result) {
+            return createError(res, 404, "Player not found.");
+        }
 
-router.use(pitStatsRoutes);
+        return res.send(result);
+    }
+);
+
+interface UpdateStatsBody {
+    type: string;
+    data: unknown;
+}
+
+router.put(
+    "/:uuid",
+    adminApiKeyAuth,
+    async ({ body, params: { uuid } }, res) => {
+        try {
+            const { data, type } = body as UpdateStatsBody;
+
+            const result = await types[type.toLowerCase()].update({
+                where: {
+                    playerUuid: uuid,
+                },
+                data,
+            });
+
+            return res.send(result);
+        } catch (err) {
+            return createError(
+                res,
+                409,
+                "Incorrect JSON body. Please refer to the documentation for the proper JSON body for this route."
+            );
+        }
+    }
+);
 
 export const thePitRoutes = router;
