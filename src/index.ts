6913f0e243cb85e78e758fa6b3b9e25948e57823
Esteban Garcia <estebangarcia2121@gmail.com>
2021-01-16 20:30:26 -0800

    Registered private routes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import { prisma } from "./prisma";
 import { masterRouter } from "./routes";
+import { privateRoutes } from "./routes/private";
 
 const main = async () => {
     const app = express();
 
     app.use(express.json());
 
+    app.use("/", privateRoutes);
     app.use("/api/v1", masterRouter);
 
     app.listen(4000, () => console.log("Started on http://localhost:4000"));
